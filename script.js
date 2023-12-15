147fe8e6f9a737e23c38b366455b46b03cc4e2ae
Thunder Bueno <felypebueno2004@gmail.com>
2023-12-14 23:58:16 -0300

    Correção de erros quase concluído

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-function WebSite_theme(){
-  const html = document.documentElement;
+const body = document.body
+const lightTheme = "light"
+const darkTheme = "dark"
+const themeKey = 'THEME_KEY'
 
-  html.classList.toggle('light');
+function toggleImageSrc(refImg, isInLightMode) {
 
-  // Pegar a tag img
-  const img = document.querySelector("#profile img");
+  // Se tiver light mode, adicionar a imagem light.
+  refImg.setAttribute(
+    "src",
+    isInLightMode
+      ? "/assets/Felipe_Light-Mode.png"
+      : "/assets/Felipe_Dark-Mode.png"
+  )
 
-  // Condição para substituir a imagem de perfil.
-  if(html.classList.contains('light')) {
-    // Se tiver light mode, adicionar a imagem light.
-    img.setAttribute('src', './assets/Felipe_Light-Mode.png');
-  } else {
-    // Se tiver dark mode, manter a imagem normal.
-    img.setAttribute('src', './assets/Felipe_Dark-Mode.png');
-  }
 }
+
+function changeTheme() {
+  // Adiciona ou remove a classe light ao body
+  body.classList.toggle(lightTheme)
+
+  // Verifica se está no light theme ou não
+  const themeValue = body.classList.contains(lightTheme)
+
+  // Adiciona ao localStorage, o thema escolhido 
+  localStorage.setItem(themeKey, themeValue ? lightTheme : darkTheme)
+
+  // Returna qual o tema selecionado
+  return themeValue
+}
+
+function ThemeProvider() {
+  // Veriricar se existe a chave theme no localStorage
+  const hasThemeKey = localStorage.getItem(themeKey)
+
+  // Se for diferente de null, quer dizer que  possui valor de dark ou light
+  if(hasThemeKey) {
+    // Se  for light, adicionar lightTheme no body, caso contrário, remover
+    hasThemeKey === lightTheme ? body.classList.add(lightTheme) :
+    body.classList.remove(lightTheme)
+  } else {
+    localStorage.setItem(themeKey, darkTheme)
+  }
+  
+  // Adicionar a primeira Image
+  const img = document.querySelector("#profile img")
+  toggleImageSrc(img)
+
+  const toggleButton = document.querySelector("#switch")
+  // Mudar o Thema e a Imagem
+  toggleButton.addEventListener("click", () => {
+    // Toggle ligth class added on body
+    const isInLightMode = changeTheme()
+
+    // Toggle Img
+    toggleImageSrc(img, isInLightMode)
+  })
+}
+
+// Initialize the App
+ThemeProvider()
